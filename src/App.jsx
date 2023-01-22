7459605ed549f886778411afad397a355b520fcb
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-01-22 10:57:39 -0600

    Aplicación final

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Buscador } from "./Buscador";
+import { GridGifs } from "./GridGifs";
+import { useSearchGifs } from "./useSearchGifs";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //Estado
+  const { valorInput, onChange, onSubmit, gifs, estaCargando } = useSearchGifs()
 
+  //JSX
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Buscador
+        valorInput={valorInput}
+        onChange={onChange}
+        onSubmit={onSubmit}
+      />
+
+      {
+        estaCargando ?
+          (<h2>Cargando...</h2>) :
+          (
+            <GridGifs
+              gifs={gifs}
+            />
+          )
+      }
+
     </div>
   )
 }
---
+++ src/useSearchGifs.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+export const useSearchGifs = () => {
+
+    const [valorInput, setValorInput] = useState('');
+    const [gifs, setGifs] = useState([]);
+    const [estaCargando, setEstaCargando] = useState(false);
+
+    const onChange = (evento) => {
+        const valor = evento.target.value;
+        setValorInput(valor);
+    }
+
+    const getGifs = async (query) => {
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=4uJ3lHbiURpxqpJHB8tIctuAqX1SluLq&q=${query}`;
+        setEstaCargando(true);
+        const response = await fetch(url);
+        const data = await response.json();
+        setEstaCargando(false);
+        console.log(data)
+        return data.data;
+    }
+
+    const onSubmit = async (evento) => {
+        evento.preventDefault()
+        const gifs = await getGifs(valorInput)
+        setGifs(gifs)
+    }
+
+    return {
+        onSubmit,
+        onChange,
+        valorInput,
+        gifs,
+        estaCargando
+    }
+}
